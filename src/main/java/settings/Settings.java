4430723aff3f3ef16f11c469d01f6138674a157b
Lancelot <lotcelan@gmail.com>
2025-01-13 20:13:40 +0100

    [+] Mode rainbow!

--- src/main/java/settings/Settings.java
+++ src/main/java/settings/Settings.java
@@ -2,11 +2,11 @@ package settings;
 
 public class Settings {
 
-    public static final int WIDTH = 1920;
-    public static final int HEIGHT = 1920;
+    public static final int WIDTH = 1200;
+    public static final int HEIGHT = 1200;
 
-    public static final int ROWS = 200;
-    public static final int COLS = 200;
+    public static final int ROWS = 300;
+    public static final int COLS = 300;
 
     public static final int CELL_SIZE = WIDTH / COLS;
 
--- src/main/java/strategies/draw/DoublePendulumPosNegDrawStrategy.java
+++ src/main/java/strategies/draw/DoublePendulumPosNegDrawStrategy.java
@@ -17,17 +17,26 @@ public class DoublePendulumPosNegDrawStrategy extends DrawStrategy {
     public void draw(Grid grid) {
         for (int i = 0; i < grid.getRows(); i++) {
             for (int j = 0; j < grid.getCols(); j++) {
+
+                    //[math.floor(127 + radius * math.cos(phi) * math.sin(theta)),
+                    //                        math.floor(127 + radius * math.sin(phi) * math.sin(theta)),
+                    //                        math.floor(127 + radius * math.cos(theta))]                try {
                 try {
-                    DoublePendulum pendulum = (DoublePendulum) grid.getCell(i, j).getPhysicalObject();
-                    if (pendulum.getAngle1() <=  Math.PI && pendulum.getAngle2() <=  Math.PI) {
-                        sketch.fill((int)(255 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),(int)(255 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),0);
-                    } else if (pendulum.getAngle1() >  Math.PI && pendulum.getAngle2() <=  Math.PI) {
-                        sketch.fill(0,0,(int)(255 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI));
-                    } else if (pendulum.getAngle1() >  Math.PI && pendulum.getAngle2() >  Math.PI) {
-                        sketch.fill(0,(int)(255 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),0);
-                    } else {
-                        sketch.fill((int)(255 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),0,0);
-                    }
+                     DoublePendulum pendulum = (DoublePendulum) grid.getCell(i, j).getPhysicalObject();
+                    sketch.fill((int)(127 + 127 * Math.cos(pendulum.getAngle1()) * Math.sin(pendulum.getAngle2())),
+                            (int)(127 + 127 * Math.sin(pendulum.getAngle1()) * Math.sin(pendulum.getAngle2())),
+                            (int)(127 + 127 * Math.cos(pendulum.getAngle2())));
+
+//                    if (pendulum.getAngle1() <=  Math.PI && pendulum.getAngle2() <=  Math.PI) {
+                //
+//                        sketch.fill((int)(100 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),(int)(0 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),100);
+//                    } else if (pendulum.getAngle1() >  Math.PI && pendulum.getAngle2() <=  Math.PI) {
+//                        sketch.fill(0,0,(int)(255 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI));
+//                    } else if (pendulum.getAngle1() >  Math.PI && pendulum.getAngle2() >  Math.PI) {
+//                        sketch.fill((int)(50 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),(int)(50 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),(int)(255 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI));
+//                    } else {
+//                        sketch.fill((int)(150 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),(int)(50 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI),(int)(75 *((pendulum.getAngle2() + pendulum.getAngle1()) % Math.PI) / Math.PI));
+//                    }
                     sketch.rect(j * Settings.CELL_SIZE, i * Settings.CELL_SIZE, Settings.CELL_SIZE, Settings.CELL_SIZE);
                 } catch (Exception e) {
                     continue;
